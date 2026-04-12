c74d70696a877891806975ad5fe7e143ac8209f9
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2026-04-12 15:50:43 +0200

    Implement vehicle addition and listing functions
    Added functions to add and list vehicles in the database.

--- Logistique_Pro/utils/vehicles.py
+++ Logistique_Pro/utils/vehicles.py
@@ -1,1 +1,25 @@
-# Placeholder file
+from utils.database import get_connection
+
+
+def add_vehicle(registration, brand, model):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO vehicles (registration, brand, model)
+        VALUES (?, ?, ?)
+    """, (registration, brand, model))
+
+    conn.commit()
+    conn.close()
+
+
+def list_vehicles():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM vehicles ORDER BY registration")
+    rows = cur.fetchall()
+
+    conn.close()
+    return rows
