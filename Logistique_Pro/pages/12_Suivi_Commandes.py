8024998a7ea829ae5fae132384451816ce477d83
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2026-04-19 12:03:37 +0200

    Delete Logistique_Pro/pages/12_Suivi_Commandes.py

--- Logistique_Pro/pages/12_Suivi_Commandes.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from utils.helpers import page_header
-
-page_header("Suivi Commandes", "Page structurée prête pour GitHub.")
-st.info("Base exécutable. Module métier à enrichir dans les prochains commits.")
